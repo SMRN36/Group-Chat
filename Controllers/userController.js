8b09ae794d0c30c49139a51cf57e93b0de2f3971
simran bhandari <simranbhandari036@gmail.com>
2023-05-23 19:50:19 +0530

    login backend

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -5,6 +5,10 @@ const sequelize = require("../Utils/database");
 const Sib = require("sib-api-v3-sdk");
 const { Op } = require("sequelize");
 
+function generateAccessToken(id, email) {
+    return jwt.sign({ userId: id, email: email }, process.env.TOKEN);
+}
+
 const getLoginPage = async (req, res, next) => {
   try {
     res.sendFile(path.join(__dirname, "../", "public", "views", "login.html"));
@@ -48,10 +52,50 @@ const postUserSignUp = async (req, res, next) => {
     } catch (error) {
       console.log(error);
     }
-  };
+};
+
+const postUserLogin = async (req, res, next) => {
+    try {
+      const email = req.body.loginEmail;
+      const password = req.body.loginPassword;
+  
+      await User.findOne({ where: { email: email } }).then((user) => {
+        if (user) {
+          bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+              return res
+                .status(500)
+                .json({ success: false, message: "Something went Wrong!" });
+            }
+            if (result == true) {
+              return res.status(200).json({
+                success: true,
+                message: "Login Successful!",
+                token: generateAccessToken(user.id, user.email),
+              });
+            } else {
+              return res.status(401).json({
+                success: false,
+                message: "Incorrect Email or Password!",
+              });
+            }
+          });
+        } else {
+          return res.status(404).json({
+            success: false,
+            message: "User doesn't Exists!",
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+};
+
   
 module.exports = {
     getLoginPage,
     postUserSignUp,
+    postUserLogin,
 };
 
