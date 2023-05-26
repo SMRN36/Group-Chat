211575b9d2016aca8d1891d74ad8dbedf21085f3
simran bhandari <simranbhandari036@gmail.com>
2023-05-26 14:54:10 +0530

    store msgs in local storage

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -2,6 +2,7 @@ const path = require("path");
 const User = require("../Models/userModel");
 const Chat = require("../Models/chatModel");
 const sequelize = require("../Utils/database");
+const { Op } = require("sequelize");
 
 exports.sendMessage = async (req, res, next) => {
   try {
@@ -19,7 +20,14 @@ exports.sendMessage = async (req, res, next) => {
 
 exports.getMessages = async (req, res, next) => {
   try {
-    const messages = await Chat.findAll();
+    const param = req.params.param;
+    const messages = await Chat.findAll({
+      where: {
+        id: {
+          [Op.gt]: param,
+        },
+      },
+    });
     return res.status(200).json({ messages: messages });
   } catch (error) {
     console.log(error);
