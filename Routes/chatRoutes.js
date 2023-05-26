64e48275513bbab30d5b301d19dd6a0c774910cd
simran bhandari <simranbhandari036@gmail.com>
2023-05-26 13:50:28 +0530

    getting msgs from database

--- Routes/chatRoutes.js
+++ Routes/chatRoutes.js
@@ -4,5 +4,6 @@ const chatController = require("../Controllers/chatController");
 const userAuthentication = require("../Middlewares/auth");
 
 router.post("/sendMessage", userAuthentication, chatController.sendMessage);
+router.get("/getMessages", chatController.getMessages);
 
 module.exports = router;
